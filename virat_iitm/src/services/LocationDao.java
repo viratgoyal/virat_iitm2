328508dac1f4c29e733ab35a036df8cf1326bfab
viratgoyal <viratgoyal@gmail.com>
2016-06-12 03:40:05 +0530

    second commit

---
+++ virat_iitm/src/services/LocationDao.java
@@ -0,0 +1,68 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class LocationDao {
+
+	private static String dbUrl = "jdbc:mysql://127.0.0.1:3306/db";
+	private static String dbUsername = "root";
+	private static String dbPassword = "virat8536";
+
+	Connection connection = null;
+	Statement stmt = null;
+	
+	public LocationDao() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+			System.out.println("connection created");
+			//connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void enterCustomerCordinates(String latitude, String longitude, String orderId) throws SQLException
+	{
+		
+		stmt = connection.createStatement();
+	      
+	      String sql = "INSERT INTO customer_coordinate " +
+	                   "VALUES ("+orderId+", '"+latitude+"', '"+longitude+"')";
+	      stmt.executeUpdate(sql);
+	}
+	
+	public String getRiderId(String orderId) throws SQLException
+	{
+		String riderId="";
+		stmt = connection.createStatement();
+	      
+	      String sql = "Select rider_id from order_details " +"where (order_id="+orderId+")";
+	      ResultSet rs = stmt.executeQuery(sql);
+	      rs.next();
+	      riderId = rs.getString(1);
+	      System.out.println("riderId = "+riderId);
+	      return riderId;
+	}
+
+	public String getCustomerNumber(String orderId) throws SQLException
+	{
+		String customerNumber="";
+		stmt = connection.createStatement();
+	      
+	      String sql = "Select cutomer_contact from order_details " +"where (order_id="+orderId+")";
+	      ResultSet rs = stmt.executeQuery(sql);
+	      rs.next();
+	      customerNumber = rs.getString(1);
+	      System.out.println("riderId = "+customerNumber);
+	      return customerNumber;
+	}
+}
--- virat_iitm/src/services/LocationService.java
+++ virat_iitm/src/services/LocationService.java
@@ -3,40 +3,74 @@ package services;
 import java.io.IOException;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import com.way2sms.SMS;
+
+import java.io.BufferedReader;
+
+import java.io.InputStreamReader;
+
+import java.io.OutputStreamWriter;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.net.URLEncoder;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @Path("/UserService")
 public class LocationService {
 
- //  UserDao userDao = new UserDao();
+	LocationDao lDao;
 
-   @GET
-   @Path("/users")
-   @Produces(MediaType.APPLICATION_XML)
-   public String getUsers(){
-	   
-	// Replace DEMO_USERNAME with username of your account
-			String username = "9600094441";
-			// Replace DEMO_PASSWORD with password of your account
-			String password = "44cc73";
-			// Replace TARGET_MOBILE with a valid mobile number
-			String toMobile = "9897133506";
+	public LocationService() {
+		lDao = new LocationDao();
+		System.out.println("LocationDao object created");
+	}
 
-			String toMessage = "Test message from http://cooljapps.blogspot.com";
+	@GET
+	@Path("/cutomerSms")
+	public String sendSMSToCustomer(@QueryParam("order_id") String orderId) throws IOException, SQLException {
 
-			FullOnSMS fullOnSMS = new FullOnSMS(username, password);
-			try {
-				if (fullOnSMS.isLoggedIn() && fullOnSMS.sendSMS(toMobile, toMessage)) {
-					fullOnSMS.logoutSMS();
-					System.out.println("Message was sent successfully ");
-				}
-			} catch (IOException e) {
-				System.out.println("Unable to send message, possible cause: " + e.getMessage());
-			}
-	   
-      return "hello world";
-   }	
+		lDao.getCustomerNumber(orderId);
+		
+		//add code to send sms to customer
+		
+		return "smsSentToCustomer";
+	}
+
+	@Path("/setCoordinates")
+	@GET
+	public String setCoordinates(@QueryParam("lat") String lat, @QueryParam("lon") String lon,
+			@QueryParam("orderId") String orderId) throws IOException, SQLException {
+
+		System.out.println("service hit");
+		System.out.println(lat);
+		System.out.println(orderId);
+		
+		lDao.enterCustomerCordinates(lat, lon, orderId);
+		
+		sendSMSToRider(lat,lon,orderId);
+		
+		return "hello world";
+
+	}
+	
+	public void sendSMSToRider(String lat,String lon,String orderId) throws SQLException
+	{
+		lDao.getRiderId(orderId);
+		
+		//add code for sending sms to rider
+		
+		System.out.println("sms sent to rider");
+	}
 }
